5f8df44e3e1be7ef811a361b088bc2d3f584db37
Kolade <101372041+marskidris@users.noreply.github.com>
2025-09-10 09:49:43 -0700

    Update build outputs and add SourceLink support
    Regenerated build artifacts and updated assembly informational version. Added App1.sourcelink.json to enable SourceLink support for improved debugging and source code mapping.

--- obj/Debug/net8.0-windows/App1.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/App1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ebe85ea255d0c3019770d7991ae7df3111c53cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("App1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
